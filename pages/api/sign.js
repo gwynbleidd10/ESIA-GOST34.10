3c6146543a177c3fc8d112a0ea43b4c3e8ebee99
Vadim Rudykh <gwynbleidd10@gmail.com>
2021-12-01 04:23:17 +0900

    test

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -8,9 +8,7 @@ export default async (req, res) => {
 
   let fileName = `/tmp/${uuidv4()}`
 
-  // let cmd = `docker run --rm -v /tmp/:/API/tmp/ -v /keys:/API/keys -w /API rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer ./keys/cert.pem -inkey ./keys/key.pem -out .${fileName}.p7b && cat .${fileName}.p7b | base64`
   let cmd = `docker run --rm -v /tmp/:/API/tmp/ -v /keys:/API/keys -w /API rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer ./keys/cert.pem -inkey ./keys/key.pem | base64`
-  console.log(cmd)
 
   fs.writeFile(fileName, message, function (err, data) {
     if (err) {
@@ -24,12 +22,14 @@ export default async (req, res) => {
           res.status(500).json({ status: 'error', error: stderr })
         }
         else {
+          console.log(out)
           let out = stdout.replace(/[\n\r]/g, '')
           // url safe base64
           out = out.replace(/=/g, '')
           out = out.replace(/\+/g, '-')
           out = out.replace(/\//g, '_')
           // out
+          console.log(out)
           res.status(200).json({ status: 'ok', message: out })
         }
       })
