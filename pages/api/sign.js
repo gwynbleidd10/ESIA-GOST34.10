37be5819e2fc8f02fdcb2342d3acc205a2dcd559
Vadim Rudykh <gwynbleidd10@gmail.com>
2021-12-01 04:17:52 +0900

    test

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -6,9 +6,9 @@ export default async (req, res) => {
   // let { message } = req.body
   let message = 'test'
 
-  let fileName = `./tmp/${uuidv4()}`
+  let fileName = `/tmp/${uuidv4()}`
 
-  let cmd = `docker run --rm -v $(pwd):$(pwd) -w $(pwd) rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer /keys/cert.pem -inkey /keys/key.pem -out ${fileName}.p7b && cat ${fileName}.p7b | base64`
+  let cmd = `docker run --rm -v /tmp/:/API/tmp/ -v /keys:/API/keys -w /API rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer ./keys/cert.pem -inkey ./keys/key.pem -out .${fileName}.p7b && cat .${fileName}.p7b | base64`
   console.log(cmd)
 
   fs.writeFile(fileName, message, function (err, data) {
