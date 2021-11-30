44dd65cfca304e77669eb06ab6199b909733575c
Vadim Rudykh <gwynbleidd10@gmail.com>
2021-12-01 04:01:47 +0900

    test

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -1,29 +1,37 @@
 const { v4: uuidv4 } = require('uuid')
 const { exec } = require('child_process')
+const fs = require('fs')
 
 export default async (req, res) => {
-  let { message } = req.body
+  // let { message } = req.body
+  let message = 'test'
 
-  let fileName = `./tmp/${uuidv4()}`
-  console.log(fileName)
+  let fileName = `/tmp/${uuidv4()}`
 
-  // let cmd = `"docker run --rm -v $(pwd):$(pwd) -w $(pwd) rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer /keys/cert.pem -inkey /keys/key.pem -out ${fileName}.p7b && cat ${fileName}.p7b | base64"`
-  let cmd = `docker run --rm -v $(pwd):$(pwd) -w $(pwd) rnix/openssl-gost cat ${fileName}`
+  let cmd = `docker run --rm -v $(pwd):$(pwd) -w $(pwd) rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer /keys/cert.pem -inkey /keys/key.pem -out ${fileName}.p7b && cat ${fileName}.p7b | base64`
   console.log(cmd)
 
-  exec(cmd, (err, stdout, stderr) => {
+  fs.writeFile(fileName, message, function (err, data) {
     if (err) {
       console.log('Error', err);
-      res.status(500).json({ status: 'error', error: stderr })
+      res.status(500).json({ status: 'error', error: err })
     }
     else {
-      let out = stdout.replace(/[\n\r]/g, '')
-      // url safe base64
-      out = out.replace(/=/g, '')
-      out = out.replace(/\+/g, '-')
-      out = out.replace(/\//g, '_')
-      // out
-      res.status(200).json({ status: 'ok', message: out })
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          console.log('Error', err);
+          res.status(500).json({ status: 'error', error: stderr })
+        }
+        else {
+          let out = stdout.replace(/[\n\r]/g, '')
+          // url safe base64
+          out = out.replace(/=/g, '')
+          out = out.replace(/\+/g, '-')
+          out = out.replace(/\//g, '_')
+          // out
+          res.status(200).json({ status: 'ok', message: out })
+        }
+      })
     }
   })
 }
