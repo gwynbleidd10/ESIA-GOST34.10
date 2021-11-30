18d30ec2a5b245e868271cddf219d9fc8f4fecf1
Vadim Rudykh <gwynbleidd10@gmail.com>
2021-12-01 02:48:08 +0900

    init

---
+++ pages/api/sign.js
@@ -0,0 +1,24 @@
+const { v4: uuidv4 } = require('uuid')
+const { exec } = require('child_process')
+
+export default async (req, res) => {
+  let { message } = req.body
+
+  let fileName = `/tmp/${uuidv4()}`
+  let cmd = `docker run --rm -v $(pwd):$(pwd) -w $(pwd) rnix/openssl-gost openssl smime -sign -binary -outform DER -noattr -signer keys/cert.pem -inkey keys/key.pem -out ${fileName}.p7b && cat ${fileName}.p7b | base64`
+
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.log('Error', err);
+      res.status(500).json({ status: 'error', error: stderr })
+    }
+
+    let out = stdout.replace(/[\n\r]/g, '')
+    // url safe base64
+    out = out.replace(/=/g, '')
+    out = out.replace(/\+/g, '-')
+    out = out.replace(/\//g, '_')
+    // out
+    res.status(200).json({ status: 'ok', message: out })
+  })
+}
