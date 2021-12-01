1469805630bcb2ee83012d788dc1ea15eb0b9213
Vadim Rudykh <gwynbleidd10@gmail.com>
2021-12-01 22:59:53 +0900

    test

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -1,15 +1,9 @@
-const { v4: uuidv4 } = require('uuid')
 const { exec } = require('child_process')
 
 export default (req, res) => {
   let { message } = req.body
   if (message != undefined && message.length > 0) {
-    console.log("Message: ", message)
-
-    let fileName = `/tmp/${uuidv4()}`
-    // let cmd = `echo ${message} | openssl smime -sign -binary -outform DER -noattr -signer /keys/cert.pem -inkey /keys/key.pem -out ${fileName}.p7b && cat .${fileName}.p7b | base64`
     let cmd = `echo ${message} | openssl smime -sign -binary -outform DER -noattr -signer /keys/cert.pem -inkey /keys/key.pem | base64`
-
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
         console.log('Error: ', err);
@@ -18,13 +12,10 @@ export default (req, res) => {
         })
       }
       else {
-        console.log("STDOUT: ", stdout)
-
         let out = stdout.replace(/[\n\r]/g, '')
         out = out.replace(/=/g, '')
         out = out.replace(/\+/g, '-')
         out = out.replace(/\//g, '_')
-
         res.status(200).json(out)
       }
     })
